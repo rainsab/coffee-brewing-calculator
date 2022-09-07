56baac379c54ae6235c50c45a4e240094d2a9e2e
Martin Bašniar <m.basniar@gmail.com>
2022-09-07 17:46:13 +0200

    Calculate water from coffee (yield) field

--- src/static/js/custom.js
+++ src/static/js/custom.js
@@ -3,8 +3,6 @@ let ratio = document.getElementById('ratio');
 let water = document.getElementById('water');
 let yield = document.getElementById('yield');
 
-document.getElementById('yield').disabled = true;
-
 let change;
 let sameChange;
 
@@ -12,10 +10,14 @@ beans.addEventListener('input', function(event) {
     
     if(change === 'water' || sameChange === 'water') {
         ratio.value = water.value / beans.value;
+        yield.value = 0.9 * water.value;
     }
     if(change === 'ratio' || sameChange === 'ratio') {
         water.value = beans.value * ratio.value;
         yield.value = 0.9 * water.value;
+    }
+    if(change === 'yield' || sameChange === 'yield') {
+        water.value = yield.value * 1.11 * 1.001;
     }
 
     if(change !== 'beans') {
@@ -31,11 +33,17 @@ water.addEventListener('input', function(event) {
     
     if(change === 'beans' || sameChange === 'beans') {
         ratio.value = water.value / beans.value;
+        yield.value = 0.9 * water.value;
     }
     if(change === 'ratio' || sameChange === 'ratio') {
         beans.value = water.value / ratio.value;
+        yield.value = 0.9 * water.value;
     }
-    yield.value = 0.9 * water.value;
+    if(change === 'yield' || sameChange === 'yield') {
+        water.value = yield.value * 1.11 * 1.001;
+        beans.value = water.value / ratio.value;
+    }
+    
 
     if(change !== 'water') {
         sameChange = change;
@@ -51,6 +59,10 @@ ratio.addEventListener('input', function(event) {
     }
     if(change === 'water' || sameChange === 'water') {
         beans.value = water.value / ratio.value;
+        yield.value = 0.9 * water.value;
+    }
+    if(change === 'yield' || sameChange === 'yield') {
+        beans.value = water.value / ratio.value;
     }
 
     if(change !== 'ratio') {
@@ -58,3 +70,30 @@ ratio.addEventListener('input', function(event) {
         change = 'ratio';
     }  
 })
+
+yield.addEventListener('input', function(event) {
+    
+    if(change === 'beans' || sameChange === 'beans') {
+        water.value = yield.value * 1.1111111111111111;
+        ratio.value = water.value / beans.value;
+    }
+    if(change === 'water' || sameChange === 'water') {
+        water.value = yield.value * 1.1111111111111111;
+        beans.value = water.value / ratio.value;
+    }
+    if(change === 'ratio' || sameChange === 'ratio') {
+        water.value = yield.value * 1.1111111111111111;
+        beans.value = water.value / ratio.value;
+    }
+
+    if(change !== 'yield') {
+        sameChange = change;
+        change = 'yield';
+    }  
+})
+
+//beans.value = water.value / ratio.value;
+//water.value = beans.value * ratio.value;
+//ratio.value = water.value / beans.value;
+//yield.value = 0.9 * water.value;
+//water.value = yield.value * 1.1111111111111111;
